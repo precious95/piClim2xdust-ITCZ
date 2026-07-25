3d13cd70972bc32c5276f786ddafd1dc7103aa69
preciousdata <34867761+precious95@users.noreply.github.com>
2026-07-25 15:39:48 -0700

    Update water-vapor.py

--- water-vapor.py
+++ water-vapor.py
@@ -1,42 +1,42 @@
 import xarray as xr
 import matplotlib.pyplot as plt
 
-# File paths for input NetCDF files
+# 
 file_lw_tot_toa_trop = 'fluxanom_lw_q_trop_tot_mpi_toa.nc'
 file_sw_tot_toa_trop = 'fluxanom_sw_q_trop_tot_mpi_toa.nc'
 
 file_lw_tot_sfc_trop = 'fluxanom_lw_q_trop_tot_mpi_sfc.nc'
 file_sw_tot_sfc_trop = 'fluxanom_sw_q_trop_tot_mpi_sfc.nc'
 
-# Load the NetCDF files
+# 
 data_lw_tot_toa_trop = xr.open_dataset(file_lw_tot_toa_trop)
 data_sw_tot_toa_trop = xr.open_dataset(file_sw_tot_toa_trop)
 
 data_lw_tot_sfc_trop = xr.open_dataset(file_lw_tot_sfc_trop)
 data_sw_tot_sfc_trop = xr.open_dataset(file_sw_tot_sfc_trop)
 
-# Calculate IRF values
+# Calculate water vapor 
 fluxanom_lw_trop = data_lw_tot_toa_trop['fluxanom_lw_q_trop_tot'] - data_lw_tot_sfc_trop['fluxanom_lw_q_trop_tot']
 fluxanom_sw_trop = data_sw_tot_toa_trop['fluxanom_sw_q_trop_tot'] - data_sw_tot_sfc_trop['fluxanom_sw_q_trop_tot']
 fluxanom_total_trop = fluxanom_lw_trop + fluxanom_sw_trop
 
 #-----------------------------------------------------------
 
-# File paths for input NetCDF files
+# 
 file_lw_tot_toa_strat = 'fluxanom_lw_q_strat_tot_mpi_toa.nc'
 file_sw_tot_toa_strat = 'fluxanom_sw_q_strat_tot_mpi_toa.nc'
 
 file_lw_tot_sfc_strat = 'fluxanom_lw_q_strat_tot_mpi_sfc.nc'
 file_sw_tot_sfc_strat = 'fluxanom_sw_q_strat_tot_mpi_sfc.nc'
 
-# Load the NetCDF files
+# 
 data_lw_tot_toa_strat = xr.open_dataset(file_lw_tot_toa_strat)
 data_sw_tot_toa_strat = xr.open_dataset(file_sw_tot_toa_strat)
 
 data_lw_tot_sfc_strat = xr.open_dataset(file_lw_tot_sfc_strat)
 data_sw_tot_sfc_strat = xr.open_dataset(file_sw_tot_sfc_strat)
 
-# Calculate IRF values
+# Calculate values
 fluxanom_lw_strat = data_lw_tot_toa_strat['fluxanom_lw_q_strat_tot'] - data_lw_tot_sfc_strat['fluxanom_lw_q_strat_tot']
 fluxanom_sw_strat = data_sw_tot_toa_strat['fluxanom_sw_q_strat_tot'] - data_sw_tot_sfc_strat['fluxanom_sw_q_strat_tot']
 fluxanom_total_strat = fluxanom_lw_strat + fluxanom_sw_strat
@@ -45,7 +45,7 @@ fluxanom_total_strat = fluxanom_lw_strat + fluxanom_sw_strat
 #------------------------------------------------------------------------------
 hus = fluxanom_total_trop +  fluxanom_total_strat
 
-# Save IRF values into a new NetCDF file
+# Save into a new NetCDF file
 output_file = 'hus_mpi.nc'
 fluxanom_dataset = xr.Dataset({
     'hus': hus,
